93ff17735ad713502cb341789ee293844a81a09a
Phudhinan21 <134205036+Phudhinan21@users.noreply.github.com>
2023-09-20 23:53:29 +0700

    added models controller routes

---
+++ models/place.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlaceSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = Place = mongoose.model("Place", PlaceSchema);
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const usersRoutes = require("./routes/api/users-routes");
+
+app.use("/api/users", usersRoutes);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.49kwkjl.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("mongoDB connecting...");
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
